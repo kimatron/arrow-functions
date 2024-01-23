62a2113965f459b02d1322d814677a219a6bce25
kimatron <kimmykim@gmail.com>
2024-01-23 13:55:08 +0000

    Playing with arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,19 @@
 
 // Vanilla JavaScript Function
 
+
 // Arrow Function With Parameters
-
+const addTwoNumbers = (a, b) => {
+  // Code block
+  return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3, 5)
+console.log(sum2);
 // Implicit Returns
-
+const myArse = poo => console.log(poo);
+myArse("Fart noise");
 // Returning Multiple Lines
